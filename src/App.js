e67d45c08a6287911847e838c9bfb21ae1b897d7
kamrulerp <kamrulerp@gmail.com>
2022-11-12 08:18:37 +0600

    Pass method in props

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     ]
   }
 
-  changeBookName = () => {
+  changeBookName = newBookName => {
     this.setState({
       books: [
-        {bookName: "Animal Farm", writter: "George Orwell"},
+        {bookName: newBookName, writter: "George Orwell"},
         {bookName: "The Da Vinchi Code", writter: "Dan Brown"},
         {bookName: "Metmorphosis", writter: "Franz Kafka"}
       ]
@@ -25,11 +25,16 @@ class App extends Component {
     return(
       <div className="App">
         <h1>Book List</h1>
-        <button onClick={this.changeBookName}>Change Book Name</button>
+        <button onClick={this.changeBookName.bind(this, "Nineteen Eighty-Four")}>Change Book Name</button>
 
-        <Book bookName={this.state.books[0].bookName} writter={this.state.books[0].writter} />
-        <Book bookName={this.state.books[1].bookName} writter={this.state.books[1].writter} />
-        <Book bookName={this.state.books[2].bookName} writter={this.state.books[2].writter} />
+        <Book
+         bookName={this.state.books[0].bookName} 
+         writter={this.state.books[0].writter} />
+        <Book bookName={this.state.books[1].bookName}
+         writter={this.state.books[1].writter} />
+        <Book bookName={this.state.books[2].bookName}
+         writter={this.state.books[2].writter}
+         change = {this.changeBookName.bint(this, "Nineteen 84")} />
       </div>
     );
   }
