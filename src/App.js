20e4418ceef3ad6586137ab2dcf89babe4eecf59
kamrulerp <kamrulerp@gmail.com>
2022-11-25 11:32:08 +0600

    Basic Featur and sytax END

--- src/App.js
+++ src/App.js
@@ -21,20 +21,41 @@ class App extends Component {
       ]
     });
   }
+
+  changeWithInputState = event => {
+    this.setState({
+      books: [
+        {bookName: event.target.value, writter: "George Orwell"},
+        {bookName: "The Da Vinchi Code", writter: "Dan Brown"},
+        {bookName: "Metmorphosis", writter: "Franz Kafka"}
+      ]
+    });
+  }
+
   render(){
+    const style ={
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white"
+    };
+
     return(
       <div className="App">
-        <h1>Book List</h1>
+        <h1 style={style}>Book List</h1>
         <button onClick={this.changeBookName.bind(this, "Nineteen Eighty-Four")}>Change Book Name</button>
+
+        <input type="text" onChange={this.changeWithInputState} />
 
         <Book
          bookName={this.state.books[0].bookName} 
-         writter={this.state.books[0].writter} />
+         writter={this.state.books[0].writter} 
+         inputName = {this.changeWithInputState}/>
         <Book bookName={this.state.books[1].bookName}
-         writter={this.state.books[1].writter} />
+         writter={this.state.books[1].writter}/>
         <Book bookName={this.state.books[2].bookName}
          writter={this.state.books[2].writter}
-         change = {this.changeBookName.bint(this, "Nineteen 84")} />
+         change = {this.changeBookName.bind(this, "Nineteen 84")} />
       </div>
     );
   }
--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,10 +1,12 @@
 import React from "react";
+import '../styleSheets/Book.css';
 
 const Book = props => {
     return(
-        <div>
+        <div className="Book">
             <h3 onClick={props.change}>Book Name: {props.bookName}</h3>
             <h4>Writter: {props.writter}</h4>
+            <input type="text" onChange={props.inputName} value={props.bookName} />
         </div>
     );
 }
